fb6d9c3c88480122e3b07aea6f3f89a762083f6a
chaeeun-Han <becky2525@naver.com>
2020-07-16 05:05:52 +0900

    blog app - Post

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    # 블로그에서 제목을 의미, max_length로 글자 수 제한을 둠.
+    title = models.CharField(max_length=30)
+    # 글에 들어갈 내용, TextField는 장문의 글을 받아올 수 있음.
+    content = models.TextField()
+
+    # 언제 작성되었는지
+    created = models.DateTimeField()
+
+    # 누가 작성을 했는지지, user라는 객체는 이미 django에서 제공. ForeignKey로 연결함.
+    author = models.ForeignKey(User, on_delete=True)
