0aff4b88289ffb43e5ee06a0edd4d4031e7a443d
chaeeun-Han <becky2525@naver.com>
2020-08-06 17:03:41 +0900

    main screen blog post (index)

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,5 @@
 """my_site_prj URL Configuration
+
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
 Examples:
@@ -12,10 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('',include('blog.urls')),
-    path('admin/', admin.site.urls),
+    path('', views.index),
 ]
